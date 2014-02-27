f7bf03d63c6f134cb37cfebe33e636f23e505437
Merwan Achibet <achibetmerwan@gmail.com>
2014-02-27 13:41:02 +0100

    - added UI

--- rom.js
+++ rom.js
@@ -1,6 +1,7 @@
 function Rom() {
 
 	this.data = [];
+	this.addresses = {};
 }
 
 Rom.prototype.load = function(data) {
@@ -22,91 +23,63 @@ Rom.prototype.load = function(data) {
 
 Rom.prototype.read_header = function() {
 
+	this.addresses['header'] = 0;
+	
 	// Read header
 	
-	this.constant = this.data.slice(0, 4); console.log(this.constant);
-	this.prg_rom_size = this.data[4]; console.log(this.prg_rom_size);
-	this.chr_rom_size = this.data[5]; console.log(this.chr_rom_size);
-	this.flag6 = this.data[6]; console.log(this.flag6);
-	this.flag7 = this.data[7]; console.log(this.flag7);
-	this.prg_ram_size = this.data[8]; console.log(this.prg_ram_size);
-	this.flag9 = this.data[9]; console.log(this.flag9);
-	this.flag10 = this.data[10]; console.log(this.flag10);
+	this.constant = this.data.slice(0, 4);
+	this.prg_rom_size = this.data[4];
+	this.chr_rom_size = this.data[5];
+	this.flag6 = this.data[6];
+	this.flag7 = this.data[7];
+	this.prg_ram_size = this.data[8];
+	this.flag9 = this.data[9];
+	this.flag10 = this.data[10];
 	
 	// Parse flags
 	
-	this.has_sram = (this.flag6 & 1<<1) !== 0; console.log(this.has_sram);
-	this.has_trainer = (this.flag6 & 1<<2) !== 0; console.log(this.has_trainer);
-	this.has_playchoice = (this.flag7 & 1<<1) !== 0; console.log(this.has_playchoice);
-	this.is_ntsc = (this.flag9 & 1) !== 0; console.log(this.is_ntsc);
+	this.has_sram = (this.flag6 & 1<<1) !== 0;
+	this.has_trainer = (this.flag6 & 1<<2) !== 0;
+	this.has_playchoice = (this.flag7 & 1<<1) !== 0;
+	this.is_ntsc = (this.flag9 & 1) !== 0;
 	
 	return 16;
 };
 
 Rom.prototype.read_trainer = function(pos) {
 
-	var size = this.has_trainer ? 512 : 0;
+	this.addresses['trainer'] = pos;
 	
-	console.log(this.data.slice(pos, pos + size));
+	//
 
-	return pos + size;
+	return pos + this.has_trainer ? 512 : 0;
 };
 
 Rom.prototype.read_prg = function(pos) {
 	
-	var size = this.prg_rom_size * 16384;
+	this.addresses['prg'] = []
+	for (var i = 0; i < this.prg_rom_size; ++i) {
+		this.addresses['prg'].push(pos + i * 16384);
+	}
 	
-	console.log(this.data.slice(pos, pos + size));
+	//
 
-	return pos + size;
+	return pos + this.prg_rom_size * 16384;
 };
-
-var colors = [
-	[0, 0, 0],
-	[50, 50, 50],
-	[150, 150, 150],
-	[250, 250, 250]
-];
  
 Rom.prototype.read_chr = function(pos) {
 
-	var size = this.chr_rom_size * 8192;
-	
-	console.log(this.data.slice(pos, pos + size));
-
-	for (var i = pos; i < pos + size; i += 16) {
-	
-		var canvas = document.createElement('canvas');
-		canvas.width = 8;
-		canvas.height = 8;
-		document.body.appendChild(canvas);
-		
-		var ctx = canvas.getContext('2d');
-		var pixels = ctx.createImageData(canvas.width, canvas.height);
-		
-		var pattern0 = this.data.slice(i, i+8);
-		var pattern1 = this.data.slice(i+8, i+16);
-		
-		for (var x = 0; x < 8; ++x) {
-			for (var y = 0; y < 8; ++y) {
-				var bit0 = (pattern0[y] >> (7-x)) & 1;
-				var bit1 = (pattern1[y] >> (7-x)) & 1;
-				var color = bit0 + (bit1 << 1);
-				pixels.data[(x + y*8)*4] = colors[color][0];
-				pixels.data[(x + y*8)*4 + 1] = colors[color][1];
-				pixels.data[(x + y*8)*4 + 2] = colors[color][2];
-				pixels.data[(x + y*8)*4 + 3] = 255;
-			}
-		}
-		
-		ctx.putImageData(pixels, 0, 0);
+	this.addresses['chr'] = []
+	for (var i = 0; i < this.chr_rom_size; ++i) {
+		this.addresses['chr'].push(pos + i * 8192);
 	}
 	
-	return pos + size;
+	return pos + this.chr_rom_size * 8192;
 };
 
 Rom.prototype.read_title = function(pos) {
 	
-	console.log(this.data.slice(pos, this.data.length));
-	console.log(pos);
+	this.addresses['title'] = pos;
+	
+	//
 };
---
+++ ui.js
@@ -0,0 +1,70 @@
+function UI(rom) {
+
+	this.rom = rom;
+	
+	this.show_prg();
+	this.show_chr();
+}
+
+UI.prototype.show_prg = function() {
+
+}
+
+UI.prototype.show_chr = function() {
+
+	for (var i = 0; i < this.rom.chr_rom_size; ++i) {
+	
+		var address = this.rom.addresses['chr'][i];
+		
+		var chr_section = document.createElement('section');
+		chr_section.setAttribute('class', 'chr');
+		
+		for (var j = 0; j < 8192; j += 16) {
+		
+			var pattern_span = document.createElement('span');
+			pattern_span.setAttribute('class', 'pattern');
+			
+			var pattern = this.rom.data.slice(address+j, address+j+16);
+			
+			pattern_span.innerHTML = pattern.map(function(x) {
+				return (x < 16 ? '0' : '') + x.toString(16).toUpperCase();
+			}).join(' ');
+			
+			var canvas = document.createElement('canvas');
+			canvas.width = 8;
+			canvas.height = 8;
+			document.body.appendChild(canvas);
+			
+			var ctx = canvas.getContext('2d');
+			var pixels = ctx.createImageData(canvas.width, canvas.height);
+			
+			var pattern0 = pattern.slice(0, 8);
+			var pattern1 = pattern.slice(8, 16);
+			
+			for (var x = 0; x < 8; ++x) {
+				for (var y = 0; y < 8; ++y) {
+					var bit0 = (pattern0[y] >> (7-x)) & 1;
+					var bit1 = (pattern1[y] >> (7-x)) & 1;
+					var color = bit0 + (bit1 << 1);
+					pixels.data[(x + y*8)*4] = colors[color][0];
+					pixels.data[(x + y*8)*4 + 1] = colors[color][1];
+					pixels.data[(x + y*8)*4 + 2] = colors[color][2];
+					pixels.data[(x + y*8)*4 + 3] = 255;
+				}
+			}
+			
+			ctx.putImageData(pixels, 0, 0);
+
+			chr_section.appendChild(pattern_span);
+		}
+		
+		document.body.appendChild(chr_section);
+	}
+}
+
+var colors = [
+	[0, 0, 0],
+	[50, 50, 50],
+	[150, 150, 150],
+	[250, 250, 250]
+];
